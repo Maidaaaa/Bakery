0846c56bb8f7b06239dc6db13963cd6416a7b0eb
Maidaaaa <z_maida@hotmail.com>
2022-04-09 13:22:22 +0200

    ✨ : animation on scroll

---
+++ main.js
@@ -0,0 +1,64 @@
+//Animation on scroll
+AOS.init({
+    duration: 800,
+    easing: "slide",
+});
+
+(function ($){
+    "use strict";
+    $(window).stellar({
+        responsive: true,
+        parallaxBackgrounds: true,
+        parallaxElements: true,
+        horizontalScrolling: false,
+        hideDistantElements: false,
+        scrollProperty: "scroll",
+    });
+
+    //full height
+    var fullHeight = function (){
+        $(".js-fullheight").css("height", $(window).height());
+        $(window).resize(function() {
+            $(".js-fullheight").css("height", $(window).height());
+        });
+    };
+    fullHeight(); //call that function
+
+    //navbar scroll
+    var scrollWindow = function(){
+        $(window).scroll(function(){
+            var $w= $(this),
+            st = $w.scrollTop(),
+            navbar = $(".ftco-navbar"),
+            sd = $(".js-scroll-wrap");
+            if (st > 150){
+                if(!navbar.hasClass("scrolled")){
+                    navbar.addClass("scrolled");
+                }
+            }
+            if(st < 150){
+                if(navbar.hasClass("scrolled")){
+                    navbar.removeClass("scrolled sleep");
+                }
+            }
+            if(st > 350){
+                if(!navbar.hasClass("awake")){
+                    navbar.addClass("awake")
+                }
+                if(sd.length > 0){
+                    sd.addClass("sleep");
+                }
+            }
+            if(st < 350){
+                if(navbar.hasClass("awake")){
+                    navbar.removeClass("awake");
+                    navbar.addClass("sleep");
+                }
+                if(sd.length >0){
+                    sd.removeClass("sleep");
+                }
+            }
+        });
+    };
+    scrollWindow();
+});
